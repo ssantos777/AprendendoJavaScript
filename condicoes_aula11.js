a0f1e2abf3cd961a29e30e86fd7ece1d88b0ea42
ssantos777 <saullo.guilherme7@gmail.com>
2022-11-19 05:12:07 -0300

    aula 11

---
+++ condicoes_aula11.js
@@ -0,0 +1,17 @@
+console.log("O console funcionou corretamente!")
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if (vel > 60 ) { //Condição Simples
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!")
+}
+
+console.log("Dirija sempre usando cinto de segurança!")
+
+var país = "Chile"
+console.log(`Vivendo em ${país}.`)
+if (país == "Brasil"){
+    console.log("Você é Brasileiro!")
+} else {
+    console.log("Você é estrangeiro!")
+}
