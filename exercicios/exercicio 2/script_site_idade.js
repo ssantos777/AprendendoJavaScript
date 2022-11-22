de5332518b4e053af22064e6ce7a7b8ca9922e9f
ssantos777 <saullo.guilherme7@gmail.com>
2022-11-22 14:38:34 -0300

    consegui

--- exercicios/exercicio 2/script_site_idade.js
+++ exercicios/exercicio 2/script_site_idade.js
@@ -1,35 +1,52 @@
-var anoInput = Number(document.querySelector("input#ano").value)
-var sexom = document.querySelector("input#sexo-m")
-var sexof = document.querySelector("input#sexo-f")
-var anoAtual = new Date().getFullYear
-var idade
-// fazer função pra quando clicar no botão;
-
-function click(){
-if (ano > anoAtual){
-    console.alert("Ano Inválido.")
-} else {
-    idade = ano - anoAtual
-}
-
-}
-
-// switch case ou if? nuncua no brasil
-
-if(sexo == x && idade < 12){
-    imagem.src = "foto-bebe-m"
-    } else if(sexo == x && idade < 12){
-        imagem.src = "foto-bebe-f"
-    } else if (sexo == x && idade < 24){
-        imagem.src = "foto-jovem-m"
-    } else if (sexo == x && idade < 24){
-        imagem.src = "foto-jovem-f"
-    }else if (sexo == x && idade < 48){
-        imagem.src = "foto-adulto-m"
-    }else if (sexo == x && idade < 48){
-        imagem.src = "foto-adulto-f"
-    }else if (sexo == x && idade < 100){
-        imagem.src = "foto-idoso-m"
-    }else if (sexo == x && idade < 100){
-        imagem.src = "foto-idoso-f"
+/* function fotos(sexo, idade, imagem){
+    if(idade < 12){
+        imagem.setAttribute("src", `foto-bebe-${sexoCod}.png`)
+    } else if(idade < 24) {
+        imagem.setAttribute("src", `foto-jovem-${sexoCod}.png`)
+    } else if(idade < 48) {
+        imagem.setAttribute("src", `foto-adulto-${sexoCod}.png`)
+    } else {
+        imagem.setAttribute("src", `foto-idoso-${sexoCod}.png`)
     }
+} */
+
+function verificar(){
+var anoInput = Number(document.querySelector("input#ano").value)
+var anoAtual = (new Date()).getFullYear()
+var idade
+var sexo = document.getElementsByName("sexo")
+var imagem = document.createElement("img")
+imagem.setAttribute("src", "imagem")
+imagem.setAttribute("id", "imagem")
+var res = document.querySelector("div#res")
+
+if (sexo[0].checked) {
+    sexo = "Homem"
+    var sexoCod = "m"
+} else {
+    sexo = "Mulher"
+    var sexoCod = "f"
+}
+
+if (anoInput == 0 || anoInput > anoAtual){
+    window.alert("Ano Inválido. Reveja os dados e tente novamente.")
+    return
+} else {
+    idade = anoAtual - anoInput 
+}
+
+if(idade < 12){
+    imagem.setAttribute("src", `foto-bebe-${sexoCod}.png`)
+} else if(idade < 24) {
+    imagem.setAttribute("src", `foto-jovem-${sexoCod}.png`)
+} else if(idade < 58) {
+    imagem.setAttribute("src", `foto-adulto-${sexoCod}.png`)
+} else {
+    imagem.setAttribute("src", `foto-idoso-${sexoCod}.png`)
+}
+
+res.innerHTML = `Detectamos ${sexo} com ${idade} anos.`
+res.appendChild(imagem)
+
+}
+
